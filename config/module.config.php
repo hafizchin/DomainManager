d8297e84b1761aa99872944f27a100dc731d0f9d
Daniel Berthereau <Daniel.github@Berthereau.net>
2019-11-11 00:00:00 +0100

    Added doctrine proxies.

--- config/module.config.php
+++ config/module.config.php
@@ -1,5 +1,13 @@
 <?php
 return [
+    'entity_manager' => [
+        'mapping_classes_paths' => [
+            dirname(__DIR__) . '/src/Entity',
+        ],
+        'proxy_paths' => [
+            dirname(__DIR__) . '/data/doctrine-proxies',
+        ],
+    ],
     'view_manager' => [
         'template_map' => [
             'config_module' => __DIR__ . '/../view/config_module.phtml',
